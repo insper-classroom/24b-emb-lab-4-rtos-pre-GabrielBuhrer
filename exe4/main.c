e36a97d9b289d6937cfb519d834d15b284da3185
GabrielBuhrer <gabrielpb1@al.insper.edu.br>
2024-09-09 15:14:04 -0300

    pre lab 4

--- exe4/main.c
+++ exe4/main.c
@@ -19,15 +19,16 @@ SemaphoreHandle_t xSemaphore_g;
 
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        xSemaphoreGiveFromISR(xSemaphore_r, 0);
+    if (gpio == BTN_PIN_R){
+        if (events == 0x4) { // fall edge
+            xSemaphoreGiveFromISR(xSemaphore_r, 0);
+        }
+    }else if (gpio == BTN_PIN_G){
+        if (events == 0x4) { // fall edge
+            xSemaphoreGiveFromISR(xSemaphore_g, 0);
+        }
     }
-}
-
-void btn_callback2(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        xSemaphoreGiveFromISR(xSemaphore_g, 0);
-    }
+    
 }
 
 void led_1_task(void *p) {
@@ -96,7 +97,7 @@ void btn_2_task(void *p) {
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
     gpio_set_irq_enabled_with_callback(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback2);
+                                       &btn_callback);
 
     int delay = 0;
     while (true) {
